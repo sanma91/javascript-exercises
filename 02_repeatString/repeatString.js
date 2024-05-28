b5f6b7487dc89fcd7f30e9ae21df071fad3397e9
sanma91 <mattiasanti1991@gmail.com>
2024-05-28 21:48:34 +0200

    a

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+    let repeatedString = "";
+    for(let i = 0; i < num; i++) {
+        repeatedString+= string
+    };
+    return repeatedString
+}
 
 // Do not edit below this line
 module.exports = repeatString;
